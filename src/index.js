b5f009eb2f06aa8f655ffbbdce6d31cdc21b786a
Yana <yana2393@gmail.com>
2023-07-04 11:59:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/tweet-test">
     <Provider store={store}>
       <App />
     </Provider>
