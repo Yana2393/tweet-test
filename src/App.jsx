0342e8077fe79400bb086de0ce73d2bcb4f4135e
Yana <yana2393@gmail.com>
2023-07-04 11:30:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home/Home';
 import { Tweets } from './components/Tweets/Tweets';
 import css from './App.module.css'
@@ -6,6 +6,9 @@ import css from './App.module.css'
 const App = ()  => {
   return (
     <div className={css.header}>
+      <Link to="/">Home</Link>
+      {/* <Link to="/tweets/:id">Tweets</Link> */}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/tweets/:id' element={<Tweets />} />
